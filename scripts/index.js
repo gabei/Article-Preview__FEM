bf75c59d999e0721c5354572606411238764ff6a
gabei <gabe1792@gmail.com>
2025-03-25 16:22:43 -0500

    feat: Begins work on buttons for share menu

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,23 @@
 console.log("Script loaded!");
+
+const shareButton = document.querySelector(".share-article");
+const previewAuthor = document.querySelector(".preview__author");
+const shareMenu = document.querySelector(".share-menu");
+const exitshareArticle = document.querySelector(".share-article-exit");
+
+
+shareButton.addEventListener('click', function() {
+  previewAuthor.classList.toggle('hide-menu');
+  shareMenu.classList.toggle('hide-menu');
+
+
+})
+
+
+
+
+// unhide menu
+// fade in menu
+
+// fade out menu
+// hide menu
