87e8b579d3b015ea65c8af7cce0048637fa25948
gabei <gabe1792@gmail.com>
2025-03-26 14:41:25 -0500

    Feat: buttons now fade share menu in and out

--- scripts/index.js
+++ scripts/index.js
@@ -7,11 +7,19 @@ const exitshareArticle = document.querySelector(".share-article-exit");
 
 
 shareButton.addEventListener('click', function() {
-  previewAuthor.classList.toggle('hide-menu');
-  shareMenu.classList.toggle('hide-menu');
+  previewAuthor.classList.remove("fade-in");
+  previewAuthor.classList.add("fade-out");
+  shareMenu.classList.remove("fade-out");
+  shareMenu.classList.add("fade-in");
+});
 
+exitshareArticle.addEventListener('click', function() {
+  shareMenu.classList.remove("fade-in");
+  shareMenu.classList.add("fade-out");
+  previewAuthor.classList.remove("fade-out");
+  previewAuthor.classList.add("fade-in");
+});
 
-})
 
 
 
