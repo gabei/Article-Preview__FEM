ef89d7f396344e750c0e6e9d4504adb306b25b10
gabei <gabe1792@gmail.com>
2025-03-26 16:30:35 -0500

    Chore: Separate click events into named functions

--- scripts/index.js
+++ scripts/index.js
@@ -5,20 +5,27 @@ const previewAuthor = document.querySelector(".preview__author");
 const shareMenu = document.querySelector(".share-menu");
 const exitshareArticle = document.querySelector(".share-article-exit");
 
+const cssAnimationTime = 2500;
 
-shareButton.addEventListener('click', function() {
+
+shareButton.addEventListener('click', fadeOutAuthorInfo);
+exitshareArticle.addEventListener('click', fadeOutShareMenu);
+
+
+
+function fadeOutAuthorInfo(){
   previewAuthor.classList.remove("fade-in");
   previewAuthor.classList.add("fade-out");
   shareMenu.classList.remove("fade-out");
   shareMenu.classList.add("fade-in");
-});
+}
 
-exitshareArticle.addEventListener('click', function() {
+function fadeOutShareMenu(){
   shareMenu.classList.remove("fade-in");
   shareMenu.classList.add("fade-out");
   previewAuthor.classList.remove("fade-out");
   previewAuthor.classList.add("fade-in");
-});
+}
 
 
 
